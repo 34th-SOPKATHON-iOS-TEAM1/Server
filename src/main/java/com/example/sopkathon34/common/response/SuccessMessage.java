4e5023dcde2a6b9214250a9286d997773fe10dd1
kgy1008 <gayeon1008@g.skku.edu>
2024-05-19 03:59:56 +0900

    [feat] 전체 메인 뷰 조회 기능 구현 완료

--- src/main/java/com/example/sopkathon34/common/response/SuccessMessage.java
+++ src/main/java/com/example/sopkathon34/common/response/SuccessMessage.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum SuccessMessage {
     SUCCESS_NOT_TODO_CREATE(HttpStatus.CREATED.value(),"Not to do 등록 성공"),
+    SUCCESS_GET_ALL_LIST(HttpStatus.OK.value(), "메인 뷰 전체 조회 성공"),
     SUCCESS_GOAL_CREATE(HttpStatus.CREATED.value(), "목표 설정 성공"),
     SUCCESS_TO_DO_CREATE(HttpStatus.CREATED.value(), "To do 등록 성공");
 
--- src/main/java/com/example/sopkathon34/service/NotTodoService.java
+++ src/main/java/com/example/sopkathon34/service/NotTodoService.java
@@ -1,21 +1,44 @@
 package com.example.sopkathon34.service;
 
+import com.example.sopkathon34.domain.Goal;
 import com.example.sopkathon34.domain.NotTodo;
+import com.example.sopkathon34.domain.Todo;
 import com.example.sopkathon34.dto.request.NotTodoCreateRequest;
+import com.example.sopkathon34.dto.response.MainViewGetResponse;
+import com.example.sopkathon34.repository.GoalRepository;
 import com.example.sopkathon34.repository.NotTodoRepository;
+import com.example.sopkathon34.repository.TodoRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class NotTodoService {
 
     private final NotTodoRepository notTodoRepository;
+    private final GoalRepository goalRepository;
+    private final TodoRepository TodoRepository;
 
     public void createNotTodo(Long memberId, NotTodoCreateRequest notTodoCreateRequest) {
         for (String content : notTodoCreateRequest.notTodoContent()) {
             NotTodo notTodo = NotTodo.create(memberId, content, false);
             notTodoRepository.save(notTodo);
         }
+    }
+
+    public MainViewGetResponse getAllView(Long memberId) {
+        Goal goal = goalRepository.findByMemberId(memberId);
+        List<NotTodo> notTodos = notTodoRepository.findByMemberId(memberId);
+        List<String> notTodoContent = notTodos.stream()
+                .map(NotTodo::getNotTodoContent)
+                .toList();
+        List<Todo> todos = TodoRepository.findByMemberId(memberId);
+        List<String> todoContent = todos.stream()
+                .map(Todo::getTodoContent)
+                .toList();
+        return new MainViewGetResponse(goal.getGoalContent(),todoContent,notTodoContent);
     }
 }
