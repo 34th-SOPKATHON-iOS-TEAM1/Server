91dbe108b3b94c699bd7b0856c9c6e143f51afcf
kgy1008 <gayeon1008@g.skku.edu>
2024-05-19 04:00:14 +0900

    [feat] DTO 수정

---
+++ src/main/java/com/example/sopkathon34/dto/response/MainViewGetResponse.java
@@ -0,0 +1,13 @@
+package com.example.sopkathon34.dto.response;
+
+import com.example.sopkathon34.domain.NotTodo;
+
+import java.util.List;
+
+public record MainViewGetResponse(
+        String goal,
+        List<String> todoContent,
+        List<String> notTodoContent
+) {
+
+}
